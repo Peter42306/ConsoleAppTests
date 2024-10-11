a468690760987021c3b3904f10f8b7e0c762b4ca
Petr Zalizko <pzalizko@gmail.com>
2024-10-11 14:37:49 +0300

    Add project files.

---
+++ ConsoleAppTests/Program.cs
@@ -0,0 +1,24 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var number in GetEvenNumbers(100))
+        {
+            Console.Write($"{number}, ");  // Output: 0, 2, 4, 6, 8, 10
+        }
+    }
+
+    // method to get even numbers up to max
+    static IEnumerable<int> GetEvenNumbers(int max)
+    {
+        for (int i = 0; i <= max; i++)
+        {
+            if (i % 2 == 0)
+            {
+                yield return i;
+            }
+        }
+    }
+}
+
